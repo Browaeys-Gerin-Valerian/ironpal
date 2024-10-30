4574cb91bac55a2e82f9058f5918669f2de1eb08
Soukina Nounou <soukina.nounou@gmail.com>
2024-10-30 11:33:01 +0100

    feat: Added all the fields needed to get a session by id

--- backend/src/models/sessionModel.ts
+++ backend/src/models/sessionModel.ts
@@ -5,9 +5,50 @@ const sessionModel = {
   async findUnique(id: number) {
     return prisma.session.findUnique({
       where: { id },
+      include: {
+        user: {
+          select: {
+            id: true,
+            firstname: true, 
+            lastname: true,
+            email: true,
+            age: true
+          },
+        },
+        muscle_group: {
+          select: {
+            id: true,
+            name: true, 
+          },
+        },
+        SessionExercise: {
+          select: {
+            id: true,
+            load: true,
+            validated: true,
+            rest_between_exercises: true,
+            exercise: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+              },
+            },
+            Set: {
+              select: {
+                id: true,
+                number_of_repetitions: true,
+                difficulty: true,
+                rest_between_sets: true,
+                session_exercise: true,
+              },
+            },
+          },
+        },
+      },
     });
   },
-
+  
   async createSession(data: { title: string, session_date: Date, validated: boolean, user_id: number, muscle_group_id: number}) {
     return prisma.session.create({
       data,
