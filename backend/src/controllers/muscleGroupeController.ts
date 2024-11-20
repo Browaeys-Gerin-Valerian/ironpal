5cb9077475c5794907e6f2c059f1003ed1fdb481
Soukina Nounou <soukina.nounou@gmail.com>
2024-11-20 14:16:27 +0100

    feat: get all muscleGroup

--- backend/src/controllers/muscleGroupeController.ts
+++ backend/src/controllers/muscleGroupeController.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
+import mucleGroupModel from '../models/muscleGroupModel';
 
 const muscleGroupController = {
-  async getMany(req: Request, res: Response) {
-    
-  }
+  async getAll(req: Request, res: Response) {
+    try {
+      const muscleGroups = await mucleGroupModel.findMany();
+      res.status(200).json(muscleGroups);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'An error occurred while fetching muscle groups.' });
+    }
+  },
 };
 
 export default muscleGroupController;
