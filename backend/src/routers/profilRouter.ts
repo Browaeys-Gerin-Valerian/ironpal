f92f5c9b69f221f24af0d20e2bef52c2e1e1a47c
Soukina Nounou <soukina.nounou@gmail.com>
2024-11-21 11:19:47 +0100

    feat: data validation for route put user

--- backend/src/routers/profilRouter.ts
+++ backend/src/routers/profilRouter.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import authMiddleware from '../middleware/security';
 import profilController from '../controllers/profilController';
+import validate from '../middleware/validation/validation';
+import schemas  from '../middleware/validation/schema/user'
 const router = express.Router();
+
+const pathSchema = schemas.path;
 
 router.get('/:id', authMiddleware, profilController.getOne)
 
-router.put('/:id', authMiddleware, profilController.update)
+router.put('/:id', authMiddleware, validate(pathSchema, 'body'), profilController.update)
 
 export default router;
