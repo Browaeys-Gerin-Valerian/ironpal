115fd94ac20385a56bfabd81e673feda36e9d999
Valerian <valgerin@hotmail.fr>
2024-11-25 17:23:40 +0100

    refacto: Add more padding to svg radio difficulty icons

--- frontend/src/components/Icons/DifficultyBorderIcon.tsx
+++ frontend/src/components/Icons/DifficultyBorderIcon.tsx
@@ -6,6 +6,7 @@ const DifficultyBorderIcon = () => {
       viewBox='0 0 24 19'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
+      style={{ marginLeft: '2px', marginRight: '2px' }}
     >
       <path
         d='M0.5 0.5H23.5V9C23.5 14.2467 19.2467 18.5 14 18.5H0.5V0.5Z'
--- frontend/src/components/Icons/DifficultyIcon.tsx
+++ frontend/src/components/Icons/DifficultyIcon.tsx
@@ -6,6 +6,7 @@ const DifficultyIcon = () => {
       viewBox='0 0 24 19'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
+      style={{ marginLeft: '2px', marginRight: '2px' }}
     >
       <path d='M0 0H24V9C24 14.5228 19.5228 19 14 19H0V0Z' fill='#13DC94' />
     </svg>
