29dad754bd1554b2bfe44cfdec20db38eebdffd0
Valerian <valgerin@hotmail.fr>
2024-12-10 11:18:59 +0100

    fix: optional props

--- frontend/src/interfaces/props/DayCardProps.ts
+++ frontend/src/interfaces/props/DayCardProps.ts
@@ -2,5 +2,5 @@ import dayjs from 'dayjs';
 import { Session } from '../data/session/Session';
 export interface DayCardProps {
     date: dayjs.Dayjs;
-    session: Session | undefined
+    session?: Session
 }
