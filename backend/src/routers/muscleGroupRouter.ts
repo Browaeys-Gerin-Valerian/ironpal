b6772df24a86ec94ab538cb419375dba1ffbc2a3
Soukina Nounou <soukina.nounou@gmail.com>
2024-12-10 14:47:25 +0100

    feat: added error handling for muscle group

--- backend/src/routers/muscleGroupRouter.ts
+++ backend/src/routers/muscleGroupRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authMiddleware from '../middleware/security';
 import muscleGroupController from '../controllers/muscleGroupeController';
+import { catchErrors } from '../middleware/handlers/errorHandlers';
 const router = express.Router();
 
 /**
@@ -13,7 +14,7 @@ const router = express.Router();
  * @returns {Error} 500 - An error has occurred and we\'re working to fix problem!
 */
 
-router.get('/', authMiddleware, muscleGroupController.getAll )
+router.get('/', authMiddleware, catchErrors(muscleGroupController.getAll))
 
 
 export default router;
