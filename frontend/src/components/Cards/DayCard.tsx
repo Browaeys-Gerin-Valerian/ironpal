f218759c56a449e2f387d863cf2230d5d0cbb7de
Valerian <valgerin@hotmail.fr>
2024-12-13 15:53:36 +0100

    delete: useless import

--- frontend/src/components/Cards/DayCard.tsx
+++ frontend/src/components/Cards/DayCard.tsx
@@ -8,7 +8,6 @@ import { DayCardProps } from '../../interfaces/props/DayCardProps';
 import { useAuthProvider } from '../../context/authContext';
 import { useNavigate } from 'react-router-dom';
 import { colorPrimary } from '../../styles/theme';
-import DescriptionCard from './DescriptionCard';
 
 const DayCard: React.FC<DayCardProps> = ({ date, session }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
